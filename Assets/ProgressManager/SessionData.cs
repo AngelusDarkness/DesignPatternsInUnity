e976623714eadc06ab754d217b90dd1fe12fe9bd
Pablo Navajas <pablo.navajas@3ogs.com.ar>
2020-04-22 17:50:45 -0300

    Added Progress Manager sample

---
+++ Assets/ProgressManager/SessionData.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System;
+
+public class SessionData {
+
+	private static GameData GAME_DATA;
+
+	private static bool LoadData() {
+        var valid = false;
+
+        var data = PlayerPrefs.GetString("data", "");
+        if (data != "") {
+	        DESEncryption.TryDecrypt(data, out var original);
+            GAME_DATA = JsonUtility.FromJson<GameData>(original);
+			GAME_DATA.LoadData();
+            valid = true;
+        } else {
+            SessionData.GAME_DATA = new GameData();
+        }
+
+        return valid;
+    }
+
+    public static bool SaveData() {
+        const bool valid = false;
+
+        try {
+            GAME_DATA.SaveData();
+            var result = DESEncryption.Encrypt(JsonUtility.ToJson(SessionData.GAME_DATA));
+            PlayerPrefs.SetString("data", result);
+            PlayerPrefs.Save();
+        } catch (Exception e) {
+            Debug.LogError(e.ToString());
+        }
+
+        return valid;
+    }
+
+    public static GameData Data {
+        get {
+			if (GAME_DATA == null)
+                LoadData();
+            return GAME_DATA;
+		}
+    }
+
+}
+
+
+[System.Serializable]
+public class GameData {
+	//Put attributes that you want to save during your game.
+    
+
+	public GameData() {
+    		
+    }
+
+	public void SaveData() {
+    
+				
+    }
+
+	public void LoadData() {
+	
+	}
+}
