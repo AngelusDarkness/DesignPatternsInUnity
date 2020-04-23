158b4d39d87cb6f10609de60eeb7cce76f4f0363
Pablo Navajas <pablo.navajas@3ogs.com.ar>
2020-04-23 10:16:42 -0300

    Updated Camera Water Effect

--- Assets/Shaders/Scripts/CameraWaterEffect.cs
+++ Assets/Shaders/Scripts/CameraWaterEffect.cs
@@ -1,14 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 [ExecuteInEditMode]
 public class CameraWaterEffect : MonoBehaviour {
 
 	[SerializeField] private Material _effectMaterial;
-
+	[Range(0, 0.1f) , SerializeField] private float _magnitude;
+	
 	void OnRenderImage(RenderTexture src, RenderTexture dst) {
 		Graphics.Blit(src, dst, _effectMaterial);
 	}
 
-
-
+	private void Update() {
+		
+		_effectMaterial.SetFloat("_Magnitude", _magnitude);
+	}
 }
